fe209d5a9f3140cfd17aefff66435c6f6e4cd1d6
tamarmakhatadze <130072068+tamarmakhatadze@users.noreply.github.com>
2023-04-08 13:33:31 +0400

    Add files via upload

---
+++ New folder (2)/page2/js.js
@@ -0,0 +1,36 @@
+function changeimage(){
+  var img = document.getElementById('image5')
+  img.src = 'img/image23.jpg';
+  img.style.borderTopRightRadius = "57px";
+}
+function changeimagea(){
+  var img = document.getElementById('image5')
+  img.src = 'img/image31.jpg';
+  img.style.borderTopRightRadius = "57px";
+}
+function changeimageb(){
+  var img = document.getElementById('image5')
+  img.src = 'img/image25.jpg';
+  img.style.borderTopRightRadius = "57px";
+}
+function changeimagec(){
+  var img = document.getElementById('image5')
+  img.src = 'img/image27.jpg'
+  img.style.borderTopRightRadius = "57px";
+}
+function changeimaged(){
+  var img = document.getElementById('image5')
+  img.src = 'img/image28.jpg';
+  img.style.borderTopRightRadius = "57px";
+}
+function changeimagee(){
+  var img = document.getElementById('image5')
+  img.src = 'img/image29.jpg';
+  img.style.borderTopRightRadius = "57px";
+}
+function changeimagef(){
+  var img = document.getElementById('image5')
+  img.src = 'img/image30.png';
+  img.style.borderTopRightRadius = "57px";
+}
+
